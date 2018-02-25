135cade35d0f0ee267cf139b14ffde6138821506
connor <connor@S340ELITE>
2018-02-25 17:57:45 -0500

    Add project files.

---
+++ TheAionProject.S1_Starter/Models/Traveler.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAionProject
+{
+    /// <summary>
+    /// the character class the player uses in the game
+    /// </summary>
+    public class Traveler : Character
+    {
+        #region ENUMERABLES
+        public enum CompanionName
+        {
+            None,
+            Mike,
+            Susan,
+            Katie,
+            Jack
+        }
+
+        #endregion
+
+        #region FIELDS
+        private string _homePlanet;
+        private CompanionName _travelerCompanionName;
+        private bool _earthBorn;
+        #endregion
+
+
+        #region PROPERTIES
+        public bool EarthBorn
+        {
+            get { return _earthBorn; }
+            set { _earthBorn = value; }
+        }
+
+        public string HomePlanet
+        {
+            get { return _homePlanet; }
+            set { _homePlanet = value; }
+        }
+
+        public CompanionName travelerCompanionName
+        {
+            get { return _travelerCompanionName; }
+            set { _travelerCompanionName = value; }
+        }
+
+
+        #endregion
+
+
+        #region CONSTRUCTORS
+
+        public Traveler()
+        {
+
+        }
+
+        public Traveler(string name, RaceType race, int spaceTimeLocationID) : base(name, race, spaceTimeLocationID)
+        {
+
+        }
+
+        #endregion
+
+
+        #region METHODS
+        public override string Greeting()
+        {
+            return $"Hello, my name is {base.Name}, I am a {base.Race}, and I am from {_homePlanet}.";
+        }
+
+
+        #endregion
+    }
+}
